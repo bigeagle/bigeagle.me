362fcb5abf5804e1d8c0c21776d01e8dac22bfec
Justin Wong <justin.w.xd@gmail.com>
2026-01-30 17:55:04 +0800

    update email address

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
   renderMathInElement(document.body, {
     delimiters: [{left: "$$", right: "$$", display: true}, {left: "$", right: "$", display: false}]
   })
-  
+
   {
   let element = document.getElementById("my-addr");
   if (element) {
-    let eaddr = "wangyuzhi" + "[at]" + "megvii.com"
+    let eaddr = "justin.w.xd" + "[at]" + "gmail.com"
       element.innerHTML = "<a href='mailto:" + eaddr.replace("[at]", "@") + "'>" + eaddr + "</a>";
     }
   };
